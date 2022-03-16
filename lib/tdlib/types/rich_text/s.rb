9d41e9c0eb6a0e308b23f733ee34d77e0c018f97
krelly <itwillnotbeasy@gmail.com>
2022-03-16 23:39:01 +0200

    Small fixes

--- lib/tdlib/types/rich_text/s.rb
+++ lib/tdlib/types/rich_text/s.rb
@@ -2,7 +2,7 @@ module TD::Types
   # A concatenation of rich texts.
   #
   # @attr texts [Array<TD::Types::RichText>] Texts.
-  class RichText::s < RichText
+  class RichText::S < RichText
     attribute :texts, TD::Types::Array.of(TD::Types::RichText)
   end
 end
